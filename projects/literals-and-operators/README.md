524416c324c0409f833f53c941d8d87ddafe6280
Noel Presti <noel@tinyBerry.com>
2023-08-30 14:54:53 -0500

    Initial add of projects files

---
+++ projects/literals-and-operators/README.md
@@ -0,0 +1,1 @@
+Dont know what I was using this project for but some good stuff none the less
---
+++ projects/scalar_example/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let result = 20;
+    let age:u32 = 20;
+    let sum:i32 = 5-15;
+    let mark:isize = 10;
+    let count:usize = 30;
+    println!("result value is {}",result);
+    println!("sum is {} abd age is {}",sum,age);
+    println!("mark is {} and count is {}",mark,count);
+
+
+
+}
